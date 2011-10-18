06b504fee56dd1ad97ca59b8ee5d149a2023ac4c
Kory Calmes <kory@calmes.org>
2011-10-18 17:29:31 -0600

    Initial Commit

---
+++ Goals5Tests/Goals5Tests.h
@@ -0,0 +1,13 @@
+//
+//  Goals5Tests.h
+//  Goals5Tests
+//
+//  Created by Kory Calmes on 10/18/11.
+//  Copyright (c) 2011 Calmes Apps. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Goals5Tests : SenTestCase
+
+@end
---
+++ Goals5Tests/Goals5Tests.m
@@ -0,0 +1,32 @@
+//
+//  Goals5Tests.m
+//  Goals5Tests
+//
+//  Created by Kory Calmes on 10/18/11.
+//  Copyright (c) 2011 Calmes Apps. All rights reserved.
+//
+
+#import "Goals5Tests.h"
+
+@implementation Goals5Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Goals5Tests");
+}
+
+@end
