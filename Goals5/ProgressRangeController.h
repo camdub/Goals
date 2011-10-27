a02fc1817f3bd4156b56e3f3b22e26dd5e0a480b
Kory Calmes <kory@calmes.org>
2011-10-27 10:23:26 -0600

    Added a UIViewController subclass to run the range election view.

---
+++ Goals5/ProgressRangeController.h
@@ -0,0 +1,13 @@
+//
+//  ProgressRangeController.h
+//  Goals5
+//
+//  Created by Kory Calmes on 10/27/11.
+//  Copyright (c) 2011 Calmes Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProgressRangeController : UIViewController
+
+@end
